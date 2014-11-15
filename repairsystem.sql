02917f960e124de8996128f456644e65d9d8fba5
azchrisf <cformeister@cox.net>
2014-11-15 10:43:50 -0700

    QFE1

--- repairsystem.sql
+++ repairsystem.sql
@@ -3,11 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 01, 2014 at 08:04 AM
+-- Generation Time: Nov 13, 2014 at 05:36 PM
 -- Server version: 5.5.40-36.1
 -- PHP Version: 5.4.23
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,7 +27,6 @@ USE `repairstatus`;
 -- Table structure for table `repairs`
 --
 
-DROP TABLE IF EXISTS `repairs`;
 CREATE TABLE IF NOT EXISTS `repairs` (
   `invid` int(11) NOT NULL,
   `status` text COLLATE utf8_unicode_ci NOT NULL,
@@ -36,15 +36,17 @@ CREATE TABLE IF NOT EXISTS `repairs` (
   `exdate` text COLLATE utf8_unicode_ci NOT NULL,
   `extime` text COLLATE utf8_unicode_ci NOT NULL,
   `comments` text COLLATE utf8_unicode_ci NOT NULL,
-  `delivertype` text COLLATE utf8_unicode_ci NOT NULL
+  `delivertype` text COLLATE utf8_unicode_ci NOT NULL,
+  `delivertime` text COLLATE utf8_unicode_ci NOT NULL,
+  `pincode` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `repairs`
 --
 
-INSERT INTO `repairs` (`invid`, `status`, `tech`, `rpdate`, `rptime`, `exdate`, `extime`, `comments`, `delivertype`) VALUES
-(12345, 'NOT STARTED', '0000 NOT ASSIGNED', '2014-11-01', '15:30', '', '', 'This is a test record and comments.', 'DELIVER TO CUSTOMER');
+INSERT INTO `repairs` (`invid`, `status`, `tech`, `rpdate`, `rptime`, `exdate`, `extime`, `comments`, `delivertype`, `delivertime`, `pincode`) VALUES
+(12345, 'COMPLETED', '1759 J. DOE', '2014-11-03', '15:30', '', '', 'Virus Service recommended.', 'DELIVER TO CUSTOMER', '17:00', '1234');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
